9a3532fc64dcce4d5f738aa90d68eb97b2bc1434
Likhitha <likhithatamma08@gmail.com>
2026-08-24 21:55:53 +0530

    Updated URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://last-mile-delivery-backend-sd06.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
@@ -36,4 +35,3 @@ api.interceptors.response.use(
 );
 
 export default api;
-
